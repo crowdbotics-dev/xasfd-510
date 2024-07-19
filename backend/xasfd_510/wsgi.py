aac02c51aadfd57381586b9aa4195c8b0a347d6c
Crowdbotics <engineering@crowdbotics.com>
2024-07-19 17:05:07 +0000

    Initial commit

---
+++ backend/xasfd_510/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for xasfd_510 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "xasfd_510.settings"
+)
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "xasfd-510",
+  url: "https://xasfd-510.botics.co",
+  api: "https://xasfd-510.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
